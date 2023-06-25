1e864696003d175913b5b5bcb8c956bc248f4dc7
crlsrr69 <110100934+crlsrr69@users.noreply.github.com>
2023-06-25 17:05:22 +0800

    NavigationDrawerHeader.js

--- NavigationDrawerHeader.js
+++ NavigationDrawerHeader.js
@@ -9,13 +9,6 @@ const NavigationDrawerHeader = (props) => {
   return (
     <View style={{flexDirection: 'row'}}>
       <TouchableOpacity onPress={toggleDrawer}>
-        <Image
-          source={{
-            uri:
-              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png',
-          }}
-          style={{width: 25, height: 25, marginLeft: 5}}
-        />
       </TouchableOpacity>
     </View>
   );
