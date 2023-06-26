ef1e6d0895059789943364533562e572a0cb52d9
crlsrr69 <110100934+crlsrr69@users.noreply.github.com>
2023-06-26 09:30:40 +0800

    App.js

--- App.js
+++ App.js
@@ -1,86 +1,32 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React, { useEffect } from 'react';
-import SplashScreen from './SplashScreen.js';
-import LoginScreen from './LoginScreen.js';
-import RegisterScreen from './RegisterScreen.js';
-import DrawerNavigationRoutes from './DrawerNavigationRoute.js';
-import mysql from 'mysql';
-
-const Stack = createStackNavigator();
-
-// Create MySQL connection
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'csorro1',
-  password: 'Srrsupremacy.11',
-  database: 'smartAniDatabase',
-  port: '3305',
-});
-
-const Auth = () => {
-  // Stack Navigator for Login and Sign up Screen
-  return (
-    <Stack.Navigator initialRouteName="LoginScreen">
-      <Stack.Screen
-        name="LoginScreen"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="RegisterScreen"
-        component={RegisterScreen}
-        options={{
-          title: 'Register',
-          headerStyle: {
-            backgroundColor: '#307ecc',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
-};
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SplashScreen from './SplashScreen';
+import HomeScreen from './HomeScreen';
 
 const App = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
   useEffect(() => {
-    // Perform a sample query
-    pool.query('SELECT * FROM users', (error, results) => {
-      if (error) { 
-        console.error('Error executing query:', error);
-      } else {
-        console.log('Query results:', results);
-      }
-    });
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 2000); 
   }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen">
-        {/* SplashScreen which will come once for 5 Seconds */}
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        {/* Auth Navigator: Include Login and Signup */}
-        <Stack.Screen
-          name="Auth"
-          component={Auth}
-          options={{ headerShown: false }}
-        />
-        {/* Navigation Drawer as a landing page */}
-        <Stack.Screen
-          name="DrawerNavigationRoutes"
-          component={DrawerNavigationRoutes}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      {showSplash ? (
+        <SplashScreen />
+      ) : (
+        <HomeScreen />
+      )}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
